d2c602d6094217fce03f12f872d8141dd0f73fb7
Isabel Limas <ilimasc3@gmail.com>
2020-02-28 09:49:57 -0500

    cookbook forms

--- controllers/cookbook.js
+++ controllers/cookbook.js
@@ -26,19 +26,61 @@ const Cookbook = require('../models/Cookbook')
  * `shopRouter`)
  */
 // const templateRouter = express.Router()
-const cookBookRouter = express.Router()
+const cookbookRouter = express.Router()
 
 /* Step 4
  * 
  * TODO: Put all request handlers here
  */
+// #NEW - GET req - take you to createForm
+cookbookRouter.get('/cookbooks/new', (req,res)=>{
+  res.render('cookbooks/createForm')
+})
+
+// #SHOW - GET req - takes you to SPECIFIC cookbook
+cookbookRouter.get('/cookbooks/:cookbookId', (req,res)=>{
+  Cookbook.findById(req.params.cookbookId).then((cookbook)=>{
+    console.log(cookbook);
+    res.render('cookbooks/cookbook',{cookbook})
+  })
+}) 
+
+// #EDIT - GET req - take you to editForm
+cookbookRouter.get('/cookbooks/:cookbookId/edit', (req, res)=>{
+  Cookbook.findById(req.params.cookbookId).then((cookbook)=>{
+    res.render('cookbooks/editForm', {cookbook})
+  })
+})
+
+// #UPDATE - PUT - update the database
+cookbookRouter.put('/cookbooks/:cookbookId',(req, res)=>{
+  // console.log('in put')
+  Cookbook.findByIdAndUpdate(req.params.cookbookId, req.body).then(()=>{
+  //  console.log("in find" +req.params.cookbookId)
+    res.redirect('/cookbooks/' + req.params.cookbookId)
+  })
+})
+
+// #DELETE - DELETE - delete an item & go back to all cookbooks
+cookbookRouter.delete('/cookbooks/:cookbookId', (req, res)=>{
+  Cookbook.findByIdAndRemove(req.params.cookbookId).then(()=>{
+    res.redirect('/cookbooks')
+  })
+})
+// #CREATE - POST req - to create/add cookbooks
+cookbookRouter.post('/cookbooks', (req, res)=>{
+  Cookbook.create(req.body).then(()=>{
+    res.redirect('/cookbooks')
+  })
+})
+
 
 /* Step 5
  *
  * TODO: delete this handler; it's just a sample
  */ 
 // templateRouter.get('/', (req, res) => {
-cookBookRouter.get('/', (req, res) => {
+cookbookRouter.get('/', (req, res) => {
   Cookbook.find().then(()=>{
     res.render('cookbooks/cookbooks')    
   })
@@ -50,4 +92,4 @@ cookBookRouter.get('/', (req, res) => {
  *
  */
 // module.exports = templateRouter;
-module.exports = cookBookRouter;
+module.exports = cookbookRouter;
